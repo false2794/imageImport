d84635f0b8bb2450be615a8be397372b2c8362f1
false2794 <christian@lemm-online.net>
2026-07-03 15:04:14 +0200

    added functionality: incident report

--- doStuff.py
+++ doStuff.py
@@ -4,18 +4,20 @@ import string
 from pathlib import Path
 
 dateFormatISO8601 = "YYYY/MM/DD"
+incidents: list[dict] = []
 
-def initImport():
+def initImport() -> None:
     logger = setLogging(__name__)
     from main import loadToml
     drives = getDrives()
     print(f"select the device from which you are planning to import images:")
     for i, drive in enumerate(drives):
         print(f"{i}: {drive}:/")
-    importDeviceLetter = drives[int(input(f"(enter the corresponding number of the desired drive): "))]
+    importDeviceLetter = drives[int(input(f"(enter the corresponding number of the desired drive): ")).strip()]
     logger.debug(f"*{sys._getframe().f_code.co_name}*: {importDeviceLetter = }")
     config = loadToml()
     copyImages(config, importDeviceLetter)
+    incidentReport(incidents)
     
 
 def getDrives() -> list:
@@ -52,14 +54,14 @@ def getFileDate(file) -> list[float|str]:
     logger.debug(f"*{sys._getframe().f_code.co_name}*: '{file}': {rawDate};  {formattedDate}")
     return [rawDate, formattedDate]
 
-def randomStringGen(length = 12, alphabet = string.ascii_letters):
+def randomStringGen(length = 12, alphabet = string.ascii_letters) -> str:
     logger = setLogging(__name__)
     import random
     randomString = "".join(random.choice(alphabet) for _ in range(length))
     logger.debug(f"*{sys._getframe().f_code.co_name}*: {randomString = }")
     return randomString
 
-def copyImages(config: dict, device: str):
+def copyImages(config: dict, device: str) -> None:
     logger = setLogging(__name__)
     import shutil
     from main import createDir
@@ -70,9 +72,10 @@ def copyImages(config: dict, device: str):
                     imageDate: str = getFileDate(pathToImage)[1]
                     targetPath = Path(config["rootDir"]).joinpath(imageDate)
                     createDir(targetPath)
-                    logger.debug(f"*{sys._getframe().f_code.co_name}*: {pathToImage} -> {targetPath.joinpath(pathToImage.name)}")
-                    shutil.copyfile(pathToImage, targetPath.joinpath(pathToImage.name))
-                    print(f"copied '{pathToImage}' to '{targetPath.joinpath(pathToImage.name)}'")
+                    if not raiseIncident(pathToImage, targetPath):
+                        logger.debug(f"*{sys._getframe().f_code.co_name}*: {pathToImage} -> {targetPath.joinpath(pathToImage.name)}")
+                        shutil.copyfile(pathToImage, targetPath.joinpath(pathToImage.name))
+                        print(f"copied '{pathToImage}' to '{targetPath.joinpath(pathToImage.name)}'")
     elif config["scheme"] == "Insanity":
         subDir = randomStringGen()
         targetPath = Path(config["rootDir"]).joinpath(subDir)
@@ -80,11 +83,47 @@ def copyImages(config: dict, device: str):
         for brand in config["validImgFileExt"]:
             for imgFileExt in config["validImgFileExt"][brand]:
                 for pathToImage in list(Path(f"{device}:/").glob(f"**/*{imgFileExt}")):
-                    logger.debug(f"*{sys._getframe().f_code.co_name}*: {pathToImage} -> {targetPath.joinpath(pathToImage.name)}")
-                    shutil.copyfile(pathToImage, targetPath.joinpath(pathToImage.name))
-                    print(f"copied '{pathToImage}' to '{targetPath.joinpath(pathToImage.name)}'")
+                    if not raiseIncident(pathToImage, targetPath):
+                        logger.debug(f"*{sys._getframe().f_code.co_name}*: {pathToImage} -> {targetPath.joinpath(pathToImage.name)}")
+                        shutil.copyfile(pathToImage, targetPath.joinpath(pathToImage.name))
+                        print(f"copied '{pathToImage}' to '{targetPath.joinpath(pathToImage.name)}'")
 
-def cleanUp(dir = "./logs"):
+def raiseIncident(pathToImage: Path, targetPath: Path) -> bool:
+    logger = setLogging(__name__)
+    import os
+    targetPathToImage = targetPath.joinpath(pathToImage.name)
+    if os.path.exists(targetPathToImage):
+        incidents.append({"src": pathToImage, "tar": targetPathToImage})
+        print(f"{pathToImage} was added to the incident Report.")
+        logger.debug(f"*{sys._getframe().f_code.co_name}*: {pathToImage} was added to the incident Report.")
+        return True
+    else:
+        return False
+
+def incidentReport(incidents: list[dict]) -> None:
+    logger = setLogging(__name__)
+    if len(incidents) > 0:
+        print(f"{len(incidents)} incidents occured while trying to import images.")
+        writeOrShow = input("Do you want to see the incident-report or shall i create a file containing the output? (show/file/both): ").strip()
+        if (writeOrShow.lower() == "show") or (writeOrShow.lower() == "both"):
+            print(f"Following files seem to already exist. None of the affected files were copied or replaced. Please check the incident and decide how to proceed:")
+            for i, v in enumerate(incidents):
+                print(f"{i+1}: '{v["src"]}' vs. '{v["tar"]}'")
+        if (writeOrShow.lower() == "file") or (writeOrShow.lower() == "both"):
+            from datetime import datetime
+            from main import createDir
+            timestamp = datetime.now().strftime("%Y%m%d_%H-%M-%S")
+            reportOutputLocation = Path(input("Please enter the desired directory for the incident output file. Defaults to the root directory of this script.: ").strip() or Path.cwd()).joinpath("incidentReports")
+            createDir(reportOutputLocation)
+            with open(reportOutputLocation.joinpath(f"report_{timestamp}.txt"), "w") as f:
+                f.write(f"{len(incidents)} incidents occured while trying to import images.\n")
+                f.write(f"Following files seem to already exist. None of the affected files were copied or replaced. Please check the incident and decide how to proceed:\n")
+                for i, v in enumerate(incidents):
+                    f.write(f"{i+1}: '{v["src"]}' vs. '{v["tar"]}'\n")
+    else:
+        print("no incidents occured. Everything should be fine. Make me a sandwich.")
+
+def cleanUp(dir = "./logs") -> None:
     logger = setLogging(__name__)
     from datetime import timedelta
     from datetime import date
@@ -101,11 +140,11 @@ def cleanUp(dir = "./logs"):
             if os.path.exists(logFile):
                 logger.debug(f"*{sys._getframe().f_code.co_name}*: deleted '{logFile}' after {today - fileDate} days with a filesize of {round(fileSize/1000, 2)} kb.")
                 os.remove(logFile)
-        if today - fileDate >= delta1 and fileSize >= size["min"]:
+        if (today - fileDate >= delta1) and (fileSize >= size["min"]):
             if os.path.exists(logFile):
                 logger.debug(f"*{sys._getframe().f_code.co_name}*: deleted '{logFile}' after {today - fileDate} days with a filesize of {round(fileSize/1000000, 2)} mb.")
                 os.remove(logFile)
-        if today - fileDate >= delta0 and fileSize >= size["max"]:
+        if (today - fileDate >= delta0) and (fileSize >= size["max"]):
             if os.path.exists(logFile):
                 logger.debug(f"*{sys._getframe().f_code.co_name}*: deleted '{logFile}' after {today - fileDate} days with a filesize of {round(fileSize/1000000, 2)} mb.")
                 os.remove(logFile)
